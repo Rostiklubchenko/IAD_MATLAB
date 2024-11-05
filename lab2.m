ce99e7aa8c97e326be64aaa4e4aa5e19638cf84c
lubch <lubchenkorostik@gmail.com>
2024-11-05 21:39:33 +0200

    Lab2

--- lab2.m
+++ lab2.m
@@ -7,16 +7,11 @@ function result = lnY(lnA, alpha, lnK, beta, lnL)
 end
 
 % 2. Генерація вхідних K і L, і вихідних Y даних зі зростанням
-
-% Розмір вибірки
 N = 20;
-
-% Генерація вхідних даних
 K = sort(randi([1, N], 1, N));
 L = sort(randi([1, N], 1, N));
 
-% Генерація вихідних даних
-Y = sort(normrnd(N, 0.05, [1, N]));
+Y = sort(normrnd(mean(K + L), 0.05 * mean(K + L), [1, N]));
 
 disp('K:');
 disp(K);
@@ -25,16 +20,26 @@ disp(L);
 disp('Y:');
 disp(Y);
 
-% 3. Побудова 3D графіку
+% 3D графік для точок даних
 figure;
 scatter3(K, L, Y, 'filled');
 xlabel('K');
 ylabel('L');
 zlabel('Y');
 grid on;
-title('3D scatter plot for K, L, Y');
+title('3D Scatter Plot with Cobb-Douglas Production Function');
 
-% 4. Побудова матриці H і перевірка її рангу
+% Створення сітки для значень K і L
+[K_grid, L_grid] = meshgrid(1:N, 1:N);
+Y_grid = X_A * (K_grid .^ X_alpha) .* (L_grid .^ X_beta);
+
+% Додавання поверхні на графік
+hold on;
+surf(K_grid, L_grid, Y_grid, 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+legend('Дані (точки)', 'Виробнича функція (поверхня)');
+hold off;
+
+% 3. Побудова матриці H і перевірка її рангу
 
 % Генерація векторів з логарифмованих значень K, L, Y
 Y_log = log(Y);
@@ -62,9 +67,10 @@ disp(['Ранг матриці H: ', num2str(rank_H)]);
 
 if rank_H ~= minSize
     disp('Матриця H неповнорангова. Переробіть вектори K і L.');
+    exit;
 end
 
-% 5. Знайти оцінки невідомих параметрів
+% 4. Знайти оцінки невідомих параметрів
 
 % Знаходження X
 X = inv(H' * H) * H' * Y_log';
@@ -72,15 +78,13 @@ X = inv(H' * H) * H' * Y_log';
 X_lnA = X(1);
 X_alpha = X(2);
 X_beta = X(3);
-
-% Отримання оцінки A з X_lnA
 X_A = exp(X_lnA);
 
 disp(['A = ', num2str(X_A)]);
 disp(['alpha = ', num2str(X_alpha)]);
 disp(['beta = ', num2str(X_beta)]);
 
-% 6. Перевірити критерій значущості коефіцієнтів регресії і визначити довірчі границі
+% 5. Перевірити критерій значущості коефіцієнтів регресії і визначити довірчі границі
 
 % Знаходження Yalt на основі отриманих параметрів
 Yalt = arrayfun(@(i) lnY(X_lnA, X_alpha, K_log(i), X_beta, L_log(i)), 1:N);
@@ -116,23 +120,20 @@ t_alpha = tinv(0.975, N - length(X)); % 95% довірчий інтервал
 
 disp(['t_alpha = ', num2str(t_alpha)]);
 
-% Обчислення довірчих границь
 T = t_alpha * sqrt(variance);
 
 for i = 1:length(T)
     disp(['T', num2str(i), ' = ', num2str(X(i)), ' +- ', num2str(T(i))]);
 end
 
-% 7. Визначити коефіцієнт множинної детермінації (R^2)
-
-% Знаходимо lnY_upper
+% 6. Визначити коефіцієнт множинної детермінації (R^2)
 lnY_upper = sum(Y_log) / N;
 
-R2 = sum((Yalt - lnY_upper) .^ 2) / sum((Y_log - lnY_upper) .^ 2);
+R2 = 1 - (sum((Y_log - Yalt) .^ 2) / sum((Y_log - lnY_upper) .^ 2));
 
 disp(['R^2 = ', num2str(R2)]);
 
-% 8. Здійснити зворотне перетворення шляхом потенціювання
+% 7. Здійснити зворотне перетворення шляхом потенціювання
 
 Y_potentiated = exp(Y_log);
 
